2ab475fec8ce8d3080bfb9ebe6749c3fdf89b9c4
Anton Shuhov <antonshuhov@gmail.com>
2023-03-02 13:53:06 +0200

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+
+import logo from "../pictures/Site-logo.png"
+import { MyStyledLinkHeader } from "./HeaderStyle";
+
+
+export function Header() {
+    return (
+        <>
+            <Box sx={{
+                bgcolor: "rgba(253,102,1,0.8)",
+                height: 80,
+                padding: 1,
+                display: "flex",
+                alignItems: "center",
+            }}>
+                <a href="https://bleach.fandom.com/ru/wiki/%D0%91%D0%BB%D0%B8%D1%87_%D0%B2%D0%B8%D0%BA%D0%B8">
+                    <img src={logo} alt="logo"/>
+                </a>
+                <Grid container spacing={2}>
+                    <Grid item md={3}>
+                        <MyStyledLinkHeader href="https://bleach.fandom.com/ru/wiki/%D0%9F%D0%B5%D1%80%D1%81%D0%BE%D0%BD%D0%B0%D0%B6%D0%B8">Персонажи</MyStyledLinkHeader>
+                    </Grid>
+                    <Grid item md={3}>
+                        <MyStyledLinkHeader href="https://bleach.fandom.com/ru/wiki/%D0%91%D0%BB%D0%B8%D1%87_%D0%B2%D0%B8%D0%BA%D0%B8:%D0%9F%D0%BE%D1%80%D1%82%D0%B0%D0%BB_%D1%81%D0%BE%D0%BE%D0%B1%D1%89%D0%B5%D1%81%D1%82%D0%B2%D0%B0">Сообщество</MyStyledLinkHeader>
+                    </Grid>
+                    <Grid item md={3}>
+                        <MyStyledLinkHeader href="https://bleach.fandom.com/ru/wiki/%D0%91%D0%BB%D0%B8%D1%87">Блич</MyStyledLinkHeader>
+                    </Grid>
+                    <Grid item md={3}>
+                        <MyStyledLinkHeader href="https://bleach.fandom.com/ru/wiki/%D0%91%D0%BB%D0%B8%D1%87_%D0%B2%D0%B8%D0%BA%D0%B8#">Исследовать</MyStyledLinkHeader>
+                    </Grid>
+                </Grid>
+            </Box>
+        </>
+    )
+}
---
+++ src/components/Header/HeaderStyle.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const MyStyledLinkHeader = styled.a`
+  color: black;
+  text-decoration: none;
+  font-size: 20px;
+  font-weight: bold;
+`;
