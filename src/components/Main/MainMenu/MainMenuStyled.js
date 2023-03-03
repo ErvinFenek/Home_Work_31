50d6071876dd9688f73177b3e171ccb43af1d7e8
Anton Shuhov <antonshuhov@gmail.com>
2023-03-03 13:06:04 +0200

    commit message

--- src/components/Main/MainMenu/MainMenuStyled.js
+++ src/components/Main/MainMenu/MainMenuStyled.js
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 
 export const MainMenuWrapper = styled.div`
-    background-color: gray;
+    //background-color: gray;
 `;
 export const MainMenuDiv = styled.div`
     width: 300px;
@@ -19,4 +19,34 @@ export const MainMenuHeader = styled.div`
 export const MainMenuLink = styled.a`
     color: lavender;
     text-decoration: none;
+`;
+export const MainMenuSmallTitle = styled.p`
+    text-align: left;
+    padding-top: 15px;
+    margin-right: 25px;
+    color: lavender;
+    font-family: Rubick-Bold;
+    font-weight: bold;
+    font-size: 20px;
+    border-bottom: 1px solid gray;
+    margin-block-start: 0;
+    margin-block-end: 0;
+`;
+
+export const MainMenuTextWrapper = styled.p`
+    padding-top: 15px;
+    padding-right: 25px;
+    line-height: 2;
+    color: lavender;
+    font-family: Rubick-Bold;
+    font-size: 16px;
+    margin-block-start: 0;
+    margin-block-end: 0;
+    text-align: left;
+`;
+
+export const MainMenuTextSpan = styled.span`
+    color: lavender;
+    font-family: Rubick-Bold;
+    font-weight: bold;
 `;
